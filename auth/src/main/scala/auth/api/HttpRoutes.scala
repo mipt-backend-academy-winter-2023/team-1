1d5d266fb2e22bffbe7c15c56c5c19e14a3f95bc
Kirill Grachev <kirill.grachoff@gmail.com>
2023-05-02 15:25:23 +0300

    Add pathes

--- auth/src/main/scala/auth/api/HttpRoutes.scala
+++ auth/src/main/scala/auth/api/HttpRoutes.scala
@@ -1,7 +1,15 @@
 package auth.api
 
+import zio.ZIO
 import zio.http._
+import zio.http.model.{Method, Status}
 
 object HttpRoutes {
-  val app: HttpApp[Any, Response] = ???
+  val app: HttpApp[Any, Response] =
+    Http.collectZIO[Request] {
+      case Method.POST -> !! / "authorization" / "login" =>
+        ZIO.succeed(Response.status(Status.NotImplemented))
+      case Method.POST -> !! / "authorization" / "register" =>
+        ZIO.succeed(Response.status(Status.NotImplemented))
+    }
 }
--- routing/src/main/scala/routing/api/HttpRoutes.scala
+++ routing/src/main/scala/routing/api/HttpRoutes.scala
@@ -1,7 +1,13 @@
 package routing.api
 
+import zio.ZIO
 import zio.http._
+import zio.http.model.{Method, Status}
 
 object HttpRoutes {
-  val app: HttpApp[Any, Response] = ???
+  val app: HttpApp[Any, Response] =
+    Http.collectZIO[Request] {
+      case Method.POST -> !! / "routes" =>
+        ZIO.succeed(Response.status(Status.NotImplemented))
+    }
 }
