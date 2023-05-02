85fd0bc65b003251f0b70211f2d329771a62f931
nikitadzhima <nikitadzhima@mail.ru>
2023-05-02 22:54:21 +0300

    task3

--- routing/src/main/scala/routing/api/HttpRoutes.scala
+++ routing/src/main/scala/routing/api/HttpRoutes.scala
@@ -1,7 +1,24 @@
 package routing.api
 
+import zio.ZIO
 import zio.http._
-
+import zio.http.model.{Method, Status}
 object HttpRoutes {
-  val app: HttpApp[Any, Response] = ???
+  val app: HttpApp[Any, Response] =
+    Http.collectZIO[Request] {
+      case req@Method.POST -> !! / "routes" => {
+        val response =
+          for {
+            ids <- ZIO.fromOption(
+              req
+                .url
+                .queryParams
+                .get("ids")
+                .flatMap(_.headOption)
+            ).tapError(_ => ZIO.logError("ids not set up"))
+          } yield Response.text("Done")
+
+        ZIO.succeed(Response.status(Status.NotImplemented))
+      }
+    }
 }
