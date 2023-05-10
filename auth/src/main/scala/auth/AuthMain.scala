a3df268be818630caa42bcea59f5db0e10e6e251
Kirill Grachev <kirill.grachoff@gmail.com>
2023-05-10 22:16:50 +0300

    Task2: Docker (#5)
    Add docker files

--- auth/src/main/scala/auth/AuthMain.scala
+++ auth/src/main/scala/auth/AuthMain.scala
@@ -1,16 +1,19 @@
 package auth
 
-import api.HttpRoutes
+import auth.api.HttpRoutes
 import auth.config.ServiceConfig
 import zio.http.Server
-import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault, http}
+import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
 
 object AuthMain extends ZIOAppDefault {
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = {
-    zio.http.Server.serve(HttpRoutes.app)
-      .provide(
-        Server.live,
-        ServiceConfig.live,
-      )
+    for {
+      _ <- ZIO.logInfo("Start AuthMain")
+      _ <- zio.http.Server.serve(HttpRoutes.app)
+        .provide(
+          Server.live,
+          ServiceConfig.live,
+        )
+    } yield ()
   }
 }
